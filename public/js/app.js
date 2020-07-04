e6f56f342efab721ffaecaa9597e67e619459264
angel99ab <40364130+angel99ab@users.noreply.github.com>
2020-07-04 20:19:28 +0200

    Add files via upload

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,6 @@
-const main_text = document.getElementById('main_text');
+const hamburguer = document.getElementById('hamburguer');
+const hamburguer_list = document.getElementById('hamburguer_list');
 
+hamburguer.addEventListener('click', () => {
+    hamburguer_list.classList.toggle('nav__list--open');
+});
