27d6ba674fc4a724edde92c70a06a8b09f852437
angel99ab <40364130+angel99ab@users.noreply.github.com>
2020-07-05 20:46:31 +0200

    Add files via upload

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,12 @@
-const hamburguer = document.getElementById('hamburguer');
-const hamburguer_list = document.getElementById('hamburguer_list');
+function hamburguer() {
+    const hamburguer = document.getElementById('hamburguer');
+    const bar = document.getElementById('bar');
+    const hamburguer_list = document.getElementById('hamburguer_list');
 
-hamburguer.addEventListener('click', () => {
-    hamburguer_list.classList.toggle('nav__list--open');
-});
+    hamburguer.addEventListener('click', () => {
+        bar.classList.toggle('hamburguer__bar--open')
+        hamburguer_list.classList.toggle('nav__list--open');
+    });
+}
+
+hamburguer();
