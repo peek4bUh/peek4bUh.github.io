5f45a284d84f02a74e3401c2d21b85a0b5c9c1c8
angel99ab <40364130+angel99ab@users.noreply.github.com>
2020-07-07 20:57:46 +0200

    Add files via upload

--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,42 @@ const hamburguer = () => {
     const bar = document.getElementById('bar');
     const hamburguer_list = document.getElementById('hamburguer_list');
 
+    const nav_home = document.getElementById('nav_home');
+    const nav_about = document.getElementById('nav_about');
+    const nav_skills = document.getElementById('nav_skills');
+    const nav_projects = document.getElementById('nav_projects');
+    const nav_contact = document.getElementById('nav_contact');
+
     hamburguer.addEventListener('click', () => {
         bar.classList.toggle('hamburguer__bar--open')
         hamburguer_list.classList.toggle('nav__list--open');
     });
+
+    nav_home.addEventListener('click', () => {
+        bar.classList.toggle('hamburguer__bar--open')
+        hamburguer_list.classList.toggle('nav__list--open');        
+    });
+
+    nav_about.addEventListener('click', () => {
+        bar.classList.toggle('hamburguer__bar--open')
+        hamburguer_list.classList.toggle('nav__list--open');        
+    });
+
+    nav_skills.addEventListener('click', () => {
+        bar.classList.toggle('hamburguer__bar--open')
+        hamburguer_list.classList.toggle('nav__list--open');        
+    });
+
+    nav_projects.addEventListener('click', () => {
+        bar.classList.toggle('hamburguer__bar--open')
+        hamburguer_list.classList.toggle('nav__list--open');        
+    });
+
+    nav_contact.addEventListener('click', () => {
+        bar.classList.toggle('hamburguer__bar--open')
+        hamburguer_list.classList.toggle('nav__list--open');        
+    });
 }
 
 hamburguer();
+
