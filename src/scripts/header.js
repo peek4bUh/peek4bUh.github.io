1161eab5624ac7cf956dfadb6b0034b58cfdd45d
peek4bUh <aandradeb99@gmail.com>
2025-06-24 11:37:44 +0200

    feat: update code with css variables and more gray ui

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,29 +1,23 @@
-const header = document.getElementById('header');
+const header = document.getElementById("header");
 let lastScroll = 0;
 
-window.addEventListener('scroll', () => {
-    const currentScroll =
-        window.pageYOffset || document.documentElement.scrollTop;
+window.addEventListener("scroll", () => {
+  const currentScroll =
+    window.pageYOffset || document.documentElement.scrollTop;
 
-    // Height shrink/expand logic
-    if (currentScroll > 0) {
-        header.classList.remove('h-[100px]');
-        header.classList.add('h-[70px]');
-        header.classList.remove('bg-(--color-bg)');
-        header.classList.add('bg-[#25282e]');
-    } else {
-        header.classList.remove('h-[70px]');
-        header.classList.add('h-[100px]');
-        header.classList.remove('bg-[#25282e]');
-        header.classList.add('bg-(--color-bg)');
-    }
+  // Height shrink/expand logic
+  if (currentScroll > 0) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
 
-    // Hide on scroll down, show on scroll up
-    if (currentScroll > lastScroll && currentScroll > 50) {
-        header.classList.add('-translate-y-full');
-    } else {
-        header.classList.remove('-translate-y-full');
-    }
+  // Hide on scroll down, show on scroll up
+  if (currentScroll > lastScroll && currentScroll > 50) {
+    header.classList.add("-translate-y-full");
+  } else {
+    header.classList.remove("-translate-y-full");
+  }
 
-    lastScroll = currentScroll <= 0 ? 0 : currentScroll;
+  lastScroll = currentScroll <= 0 ? 0 : currentScroll;
 });
